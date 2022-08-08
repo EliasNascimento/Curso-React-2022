e82ac854b4c85ab82ad6dd090fac647f3e9c613d
Elias Nascimento <eliasnasciment@hotmail.com>
2022-08-08 10:48:47 -0300

    Css e classes dinâmicas

--- 4_CSS_REACT/css/src/App.js
+++ 4_CSS_REACT/css/src/App.js
@@ -1,10 +1,61 @@
-import logo from "./logo.svg";
+import { useState } from "react";
 import "./App.css";
+import MyComponent from "./components/MyComponent";
 
 function App() {
+  const [value, setValue] = useState(0);
+
+  const changeValue = () => {
+    var randomValue = Math.floor(Math.random() * 50) - 10;
+    setValue(randomValue);
+  };
+
+  const [title, setTitle] = useState(false);
+
+  const changeClass = () => {
+    setTitle(!title);
+  };
+
   return (
     <div className="App">
+      {/** Css Global */}
       <h1>React com CSS</h1>
+
+      {/** Css do componente */}
+      <MyComponent />
+      <p>Este parágrado é do App.js</p>
+
+      {/** Css Inline */}
+      <p
+        style={{
+          color: "magenta",
+          padding: "25px",
+          borderTop: "2px solid red",
+        }}
+      >
+        Este elemento foi estilizado de forma inline
+      </p>
+
+      {/** Css Inline Dinâmico */}
+      <h1>Css Inline Dinâmico</h1>
+      <button onClick={changeValue}>Mudar valor</button>
+      <p>Valor: {value}</p>
+      <p
+        style={
+          value < 0
+            ? { color: "red", backgroundColor: "silver" }
+            : { color: "blue", backgroundColor: "silver" }
+        }
+      >
+        Se valor for menor que '0', este texto será vermelho, senão, azul.
+      </p>
+
+      {/** Classe Dinâmica */}
+      <h1>Classe Dinâmica</h1>
+      <button onClick={changeClass}>Mudar Classe</button>
+      <h2 className={title ? "red-title" : "title"}>
+        Este título tem classe dinâmica.
+      </h2>
     </div>
   );
 }
---
+++ 4_CSS_REACT/css/src/components/MyComponent.js
@@ -0,0 +1,12 @@
+import "./MyComponent.css";
+
+const MyComponent = () => {
+  return (
+    <div>
+      <h1>CSS de componente</h1>
+      <p className="my-comp-paragraph">Este é o parágrafo do componente</p>
+    </div>
+  );
+};
+
+export default MyComponent;
