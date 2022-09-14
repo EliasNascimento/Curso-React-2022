992554233268c48025ae4720d44e006e78f15d88
Elias Nascimento <eliasnasciment@hotmail.com>
2022-09-13 23:16:13 -0300

    Add Files

---
+++ REQ_HTTP/src/hooks/useFetch.js
@@ -0,0 +1,63 @@
+import { useState, useEffect } from "react";
+
+// 4 - custom hook
+
+export const useFetch = (url) => {
+  const [data, setData] = useState(null);
+
+  // 5 - refatorando o post
+  const [config, setConfig] = useState(null);
+  const [method, setMethod] = useState(null);
+  const [callFetch, setCallFetch] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const httpConfig = (data, method) => {
+    if (method === "POST") {
+      setConfig({
+        method,
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+
+      setMethod(method);
+    }
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      // 6 - loading
+      setLoading(true);
+
+      const res = await fetch(url);
+
+      const json = await res.json();
+
+      setData(json);
+
+      setLoading(false);
+    };
+
+    fetchData();
+  }, [url, callFetch]);
+
+  // 5 - refatorando o post
+  useEffect(() => {
+    const httpRequest = async () => {
+      if (method === "POST") {
+        let fetchOptions = [url, config];
+
+        const res = await fetch(...fetchOptions);
+
+        const json = await res.json();
+
+        setCallFetch(json);
+      }
+    };
+
+    httpRequest();
+  }, [config, method, url]);
+
+  return { data, httpConfig, loading };
+};
