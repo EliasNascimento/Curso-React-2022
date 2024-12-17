cfc33c5451e213e42f8664b355474f4d26433318
Elias Nascimento <eliasnasciment@hotmail.com>
2024-12-17 20:00:43 +0000

    Add project 'Avançando em React'

---
+++ 3_AVANCANDO_EM_REACT/avancando/src/App.jsx
@@ -0,0 +1,21 @@
+import "./App.css";
+import City from "./assets/city.jpg";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Avançando em React</h1>
+      {/* Imagem em public */}
+      <div>
+        <img src="/img1.jpg" alt="City" />
+      </div>
+
+      {/* Imagem em assets */}
+      <div>
+        <img src={City} alt="Paisagem" />
+      </div>
+    </div>
+  );
+}
+
+export default App;
